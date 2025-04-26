3a9397376aa8492554f250bce0e88bfefd5230b1
Shinjaeseob <zxc422523@naver.com>
2025-04-26 20:06:25 +0900

    FEAT: useFetch 파일 추가

--- src/page/Login/model/useLoginForm.jsx
+++ src/page/Login/model/useLoginForm.jsx
@@ -4,7 +4,7 @@ const useLoginForm = (validateId, validatePassword) => {
   const navigate = useNavigate()
 
   // 로그인 처리 함수
-  const handleLogin = (setErrors) => {
+  const handleLogin = async (setErrors, id, password) => {
     const idError = validateId()
     const passwordError = validatePassword()
 
@@ -20,8 +20,23 @@ const useLoginForm = (validateId, validatePassword) => {
     }
 
     if (Object.keys(errors).length === 0) {
-      alert("로그인 성공")
-      navigate('/')
+
+      const response = await fetch('http://39.123.217.25:8000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id, password}),
+      })
+
+      if (response.ok) {
+        alert("로그인 성공")
+        navigate('/')
+      } else {
+        const error = await response.json()
+        setErrors({ login: error.message })
+      }
+      
     }
   }
 
--- src/page/SignUp/model/useSendEmailCode.jsx
+++ src/page/SignUp/model/useSendEmailCode.jsx
@@ -1,37 +1,77 @@
 import { useState, useEffect } from "react"
+import useFetch from "../../../entities/model/usePost"
+import useValidatorInput from "../../../shared/model/useValidatorInput"
+import { regex } from "../../../shared/Content/regex"
 
-const useSendEmailCode = (validateEmail) => {
-  const [isEmailCodeSent, setIsEmailCodeSent] = useState(false)
-  const [generatedCode, setGeneratedCode] = useState("123456")
+const useSendEmailCode = () => {
+  // const [isEmailCodeSent, setIsEmailCodeSent] = useState(false)
   const [timer, setTimer] = useState(900)
-  const [emailCodeMessage, setEmailCodeMessage] = useState('')
+  // const [emailCodeMessage, setEmailCodeMessage] = useState('')
+
+  const [isSending, setIsSending] = useState(false)   // 이메일 전송 중인 것을 표현하기 위한 상태 ( 만약에 화면에 전송 중인 것을 보여주지 않으면 필요 없는 상태 )
+  const [isEmailSent, setIsBackendRequest] = useState(false)   // 백엔드 통신 성공 여부 상태
+  // const [emailSentResultMessage, setBackendMeesage] = useState("")   // 백엔드 통신이 실패했을 때 출력할 메세지 상태
+
+  const postData = useFetch()
 
 
-  const handleSendEmailCode = (setErrors) => {
-    const emailError = validateEmail()
+  const requestPostEmailCode = async (emailRef) => {
+    
+    const isValidateEmail = useValidatorInput(emailRef, regex.email)
 
-    if (emailError) {
-      setErrors((prev) => ({ ...prev, email: emailError }))
-      return
+    // setErrors((prev) => {
+    //   const updated = { ...prev }
+    //   delete updated.email
+    //   return updated
+    // })
+
+    // try {
+    if (!isValidateEmail) {
+
+      setIsSending(true)
+
+      const response = await postData("POST", "/auth/verify-email", { 
+        email: emailRef.current.value 
+      })
+
+      if (response.status === 400) {
+        alert("인증번호가 일치하지 않습니다.")
+        // setBackendMeesage("인증번호가 일치하지 않습니다.")
+      } 
+      else if (response.stauts === 409) {
+        alert("이미 가입된 이메일이 존재합니다.")
+        // setBackendMeesage("이미 가입된 이메일이 존재합니다.")
+      }
+      else if (response.status === 200) {
+        setIsBackendRequest(true)
+        setTimer(900)
+      }
+      
+      setIsSending(false)
     }
 
-    setErrors((prev) => {
-      const updated = { ...prev }
-      delete updated.email
-      delete updated.validateEmail
-      return updated
-    })
+      // if (!response.success) {
+      //   throw new Error(response.message || "요청 실패")
+      // }
 
-    setIsEmailCodeSent(true)
-    setEmailCodeMessage("")
-    alert(`인증 코드가 전송되었습니다.: ${generatedCode}`)
-    setTimer(900)    
+      // setIsEmailCodeSent(true)
+      // setEmailCodeMessage("")
+      // setTimer(900)
+
+      // console.log(`인증 코드가 전송되었습니다.`)
+      // console.log(response)
+    // } catch (err) {
+      // console.error("이메일 인증 코드 전송 오류:", err.message)
+      // setEmailCodeMessage("인증 코드 전송 실패: " + err.message)
+    // } finally {
+      // setIsSending(false)
+    //}
   }
 
   useEffect(() => {
     let interval
 
-    if (isEmailCodeSent && timer > 0) {
+    if (isEmailSent && timer > 0) {
       interval = setInterval(() => {
         setTimer((prevTimer) => prevTimer - 1)
       }, 1000);
@@ -41,7 +81,7 @@ const useSendEmailCode = (validateEmail) => {
     }
 
     return () => clearInterval(interval)
-  }, [isEmailCodeSent, timer])
+  }, [isEmailSent, timer])
 
   // 타이머 포맷팅 함수
   const formatTime = (time) => {
@@ -50,7 +90,8 @@ const useSendEmailCode = (validateEmail) => {
     return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
   }
 
-  return { handleSendEmailCode, isEmailCodeSent, emailCodeMessage, timer, formatTime, generatedCode }
+  // return { handleSendEmailCode, isEmailCodeSent, emailCodeMessage, timer, formatTime, isSending }
+  return { requestPostEmailCode, isSending, isEmailSent, timer, formatTime, isValidateEmail }
 }
 
 export default useSendEmailCode
